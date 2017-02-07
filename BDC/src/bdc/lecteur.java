55f97e56374a35cb4d542eca10581cb7370d1dbd
technet <ellipse.paro@gmail.com>
2017-02-07 15:00:54 +0100

    some class done, good prof base

--- BDC/src/bdc/lecteur.java
+++ BDC/src/bdc/lecteur.java
@@ -1,7 +1,144 @@
 package bdc;
 
-/**
- * Created by moran on 07/02/2017.
- */
-public class lecteur {
+import java.io.Serializable;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Iterator;
+
+// Classe de gestion de Lecteur
+
+public class Lecteur implements Serializable 
+{
+	
+	private static final long serialVersionUID = 422L;
+	
+	// -----------------------------------------------
+		//Attributs
+	// -----------------------------------------------
+	
+		private String _nom;
+		private String _prenom;
+		private Integer _numLecteur;
+		private GregorianCalendar _dateNaiss;
+		private String _adresse;
+		private String _tel;
+	
+	
+	
+	// -----------------------------------------------
+		//Constructeur
+	// -----------------------------------------------
+		
+		public Lecteur(String nom, String prenom, Integer numLecteur, GregorianCalendar dateNaiss, String adresse, String tel)
+		{
+			this.setNom(nom);
+			this.setPrenom(prenom);
+			this.setNumLecteur(numLecteur);
+			this.setDateNaiss(dateNaiss);
+			this.setAdresse(adresse);
+			this.setTel(tel);
+		}
+		
+// -----------------------------------------------
+	// Public
+// -----------------------------------------------
+		
+		// -----------------------------------------------
+			//Getters
+		// -----------------------------------------------
+	
+		public String getNom() {
+			return _nom;
+		}
+
+		public String getPrenom() {
+			return _prenom;
+		}
+
+		public Integer getNumLecteur() {
+			return _numLecteur;
+		}
+		
+		public GregorianCalendar getDateNaiss() {
+			return _dateNaiss;
+		}
+
+		public String getAdresse() {
+			return _adresse;
+		}
+
+		public String getTel() {
+			return _tel;
+		}
+		// -----------------------------------------------
+			// Methodes
+		// -----------------------------------------------
+		
+		/*
+		 * La m�thode afficherLecteur affiche l'ensemble des informations relatives � un lecteur.
+		 */
+		public void afficherLecteur()
+		{
+			System.out.println("Numero lecteur : " + this.getNumLecteur());
+			System.out.println("Nom et prenom du lecteur: " + this.getNom() + " " + this.getPrenom());
+			System.out.println("Age : " + this.calculAge() + " ans");
+			System.out.println("Adresse : " + this.getAdresse());
+			System.out.println("Telephone : " + this.getTel());
+			EntreesSorties.afficherMessage("");
+		}
+		
+		
+		/*
+		 * la m�thode calculAge permet de d�terminer l'age des lecteurs grace a leur date de naissance
+		 * et la date actuelle. De cette fa�on, il n'y a pas de mise a jour a faire sur l'age des lecteurs.
+		 */
+		public Integer calculAge() {
+			Integer age;
+			GregorianCalendar dateNaissComp;
+			GregorianCalendar dateActuelle = new GregorianCalendar();
+			dateNaissComp = new GregorianCalendar(dateActuelle.get(GregorianCalendar.YEAR), _dateNaiss.get(GregorianCalendar.MONTH), _dateNaiss.get(GregorianCalendar.DATE));
+			if(dateNaissComp.before(dateActuelle)){
+				age=dateActuelle.get(GregorianCalendar.YEAR)-_dateNaiss.get(GregorianCalendar.YEAR);
+			}
+			else{
+				age=dateActuelle.get(GregorianCalendar.YEAR)-_dateNaiss.get(GregorianCalendar.YEAR)-1;
+			}
+			return age;
+		}
+		
+	
+	
+// -----------------------------------------------
+	// Private
+// -----------------------------------------------
+
+		// -----------------------------------------------
+			//Setters
+		// -----------------------------------------------
+
+		private void setNom(String nom) {
+			this._nom = nom;
+		}
+
+		private void setPrenom(String prenom) {
+			this._prenom = prenom;
+		}
+		
+		private void setNumLecteur(Integer numLecteur) {
+			this._numLecteur = numLecteur;
+		}
+
+		private void setDateNaiss(GregorianCalendar dateNaiss) {
+			this._dateNaiss = dateNaiss;
+		}
+
+		private void setAdresse(String adresse) {
+			this._adresse = adresse;
+		}
+
+		private void setTel(String tel) {
+			this._tel = tel;
+		}
+		
+		
 }
--- BDC/src/bdc/ouvrage.java
+++ BDC/src/bdc/ouvrage.java
@@ -1,6 +1,7 @@
 package bdc;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 /**
  * Created by moran on 07/02/2017.
@@ -10,4 +11,31 @@ public class ouvrage {
     private String titre;
     private ArrayList<String> auteurs;
     private String editeur;
+    private GregorianCalendar dateP;
+    private String target;
+
+    public ouvrage(String ISBN, String titre, ArrayList<String> auteurs, String editeur, GregorianCalendar dateP, String target) {
+        this.ISBN = ISBN;
+        this.titre = titre;
+        this.auteurs = auteurs;
+        this.editeur = editeur;
+        this.dateP = dateP;
+        this.target = target;
+    }
+
+
+    public void setOuvrage(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String afficher() {
+        return "ouvrage{" +
+                "ISBN='" + ISBN + '\'' +
+                ", titre='" + titre + '\'' +
+                ", auteurs=" + auteurs +
+                ", editeur='" + editeur + '\'' +
+                ", dateP=" + dateP +
+                ", target='" + target + '\'' +
+                '}';
+    }
 }
